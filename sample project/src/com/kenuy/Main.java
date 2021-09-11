b4b9adcdf05a07b7b64fbe91dfb6960792ef8594
Ken Uy <kendrickvernon.uy@benilde.edu.ph>
2021-09-11 18:00:14 +0800

    Added Sample Project

---
+++ sample project/src/com/kenuy/Main.java
@@ -0,0 +1,19 @@
+package com.kenuy;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
